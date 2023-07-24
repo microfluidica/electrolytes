8d33bb2607882da4cbb720dcea168dcacbfbbab5
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-07-24 18:01:09 -0300

    Bump version to 0.3.0

--- electrolytes/__init__.py
+++ electrolytes/__init__.py
@@ -12,7 +12,7 @@ from pydantic import BaseModel, Field, field_validator, FieldValidationInfo, mod
 from filelock import FileLock
 from typer import get_app_dir
 
-__version__ = "0.2.4"
+__version__ = "0.3.0"
 
 
 class Constituent(BaseModel, populate_by_name=True, frozen=True):
